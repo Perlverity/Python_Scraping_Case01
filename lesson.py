5b1eb45132c707f5fa50a9028c2e9b37b967632d
Perlverity <snowman.ss.6124@gmail.com>
2021-07-13 10:53:47 +0900

    First Commit!

--- lesson.py
+++ lesson.py
@@ -5,7 +5,7 @@ headers = {
 }
 URL = 'http://www.python.org'
 
-r = requests.get(URL, headers=headers, timeout=3, allow_redirects=False)
+r = requests.get(URL, headers=headers, timeout=3)
 
 print(r.url)
 print(r.status_code)
